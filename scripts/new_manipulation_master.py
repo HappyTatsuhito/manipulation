389bc65f64de5dcb7177c430c13d518593cb034e
HappyTatsuhito <b1730096@planet.kanazawa-it.ac.jp>
2019-11-25 16:08:59 +0900

    by Laptop 11/25

--- scripts/new_manipulation_master.py
+++ scripts/new_manipulation_master.py
@@ -3,40 +3,42 @@
 
 import rospy
 import actionlib
-from std_srvs.srv import SetBool
+# ros msgs
 from geometry_msgs.msg import Twist
+# ros srvs
+from manipulation.srv import ManipulateSrv
+# action msgs
 from manipulation.msg import *
 
 class ObjectRecognizer(object):
     def __init__(self):
-        self.feedback_flg = 'None'
+        self.feedback_flg = None
 
     def recognizerFeedback(self,msg):
         rospy.loginfo('feedback %s'%(msg))
         self.feedback_flg = msg.recog_feedback
         
     def recognizeObject(self,target_name):
-        ac = actionlib.SimpleActionClient('/object/localize', ObjectRecognizerAction)
-        rospy.loginfo('waiting for recognizer server')
-        ac.wait_for_server(rospy.Duration(5))
-        rospy.loginfo('send goal')
+        act = actionlib.SimpleActionClient('/object/localize', ObjectRecognizerAction)
+        rospy.loginfo('Start action with Recognizer')
+        act.wait_for_server(rospy.Duration(5))
         goal = ObjectRecognizerGoal()
         goal.recog_goal = target_name
-        ac.send_goal(goal, feedback_cb = self.recognizerFeedback)
+        act.send_goal(goal, feedback_cb = self.recognizerFeedback)
         loop_count = 0
-        rate = rospy.Rate
-        while not ac.wait_for_result() and not rospy.is_shutdown():
-            rospy.loginfo(loop_count)
+        result = None
+        while result == None and not rospy.is_shutdown():
+            result = act.get_result()
             if self.feedback_flg:
                 loop_count = 0
-                self.feedback_flg = 'None'
+                self.feedback_flg = None
             else:
                 loop_count += 1
-                self.feedback_flg = 'None'
+                self.feedback_flg = None
             if loop_count > 9:
-                ac.cancel_goal()
-            rospy.Rate().sleep(3.0)
-        result = ac.get_result()
+                act.cancel_goal()
+            rospy.Rate(3.0).sleep()
+        result = act.get_result()
         recognize_flg = loop_count < 10
 
         return recognize_flg, result.recog_result
@@ -49,23 +51,24 @@ class ObjectGrasper(object):
         rospy.loginfo('feedback %s'%(msg))
 
     def graspObject(self, target_centroid):
-        ac = actionlib.SimpleActionClient('/object/grasp', ObjectGrasperAction)
+        act = actionlib.SimpleActionClient('/object/grasp', ObjectGrasperAction)
         rospy.loginfo('waiting for grasper server')
-        ac.wait_for_server(rospy.Duration(5))
+        act.wait_for_server(rospy.Duration(5))
         rospy.loginfo('send goal')
         goal = ObjectGrasperGoal()
         goal.recog_goal = target_centroid
-        ac.send_goal(goal, feedback_cb = self.grasperFeedback)
-        ac.wait_for_result()
-        result = ac.get_result()
+        act.send_goal(goal, feedback_cb = self.grasperFeedback)
+        act.wait_for_result()
+        result = act.get_result()
 
         return result
 
 def main(req):
     recognize_flg = False
     grasp_flg = False
+    print req.target
     OR = ObjectRecognizer()
-    recognize_flg, object_centroid = OR.recognizeObject('cup')
+    recognize_flg, object_centroid = OR.recognizeObject(req.target)
     if recognize_flg:
         OG = ObjectGrasper()
         #grasp_flg = OG.graspObject(object_centroid)
@@ -78,5 +81,5 @@ def main(req):
 if __name__ == '__main__':
     rospy.init_node('manipulation_master')
     #ros_service
-    manipulation = rospy.Service('/manipulation', SetBool, main)
+    manipulation = rospy.Service('/manipulation',ManipulateSrv, main)
     rospy.spin()
